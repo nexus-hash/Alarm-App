f6fc25e7fbf2a667fee6964d125f757f5c919dd5
Soumya Ranjan Tripathy <srtsoumya21@gmail.com>
2020-08-14 22:15:20 +0530

    addedasidemenubar

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:new_proj/clock_round.dart';
+import 'package:intl/intl.dart';
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,20 +9,87 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+
   @override
   Widget build(BuildContext context) {
+
+
+    var now = DateTime.now();
+    var formattedTime = DateFormat('HH:mm').format(now);
+    var formattedDate = DateFormat('EEE, d MMM').format(now);
+    var timeZoneString1 = now.timeZoneOffset.toString().split('.').first.split(':').first;
+    var timeZoneString =  now.timeZoneOffset.toString().split('.').first.split(':')[1];
+
+    var offsetSign = '';
+    if(!timeZoneString.startsWith('-')){
+      offsetSign = '+';
+    }
+
     return Scaffold(
       backgroundColor:  Color(0xFF2D2F41),
-      body: Container(
+      body: Row(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(onPressed: (){}, child:
+              Column(
+                children: <Widget>[
+                  FlutterLogo(),
+                ],
+              )
+              )
+            ],
+          ),
+          VerticalDivider(
+            color: Colors.white54,width: 1,
+          ),
+          Expanded(
+            child: Container(
 
-        padding: EdgeInsets.all(32),
-        child: Column(
-          children: <Widget>[
-            Text("Clock",
-            style: TextStyle(color: Colors.white,fontSize: 24),),
-            RoundClock(),
-          ],
-        ),
+              padding: EdgeInsets.symmetric(horizontal: 32,vertical: 64),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Clock",
+                  style: TextStyle(color: Colors.white,fontSize: 22),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height*.042,
+                  ),
+                  Text(formattedTime,
+                    style: TextStyle(color: Colors.white,fontSize: 54),
+                  ),
+                  Text(formattedDate,
+                    style: TextStyle(color: Colors.white,fontSize: 18),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height*.025,
+                  ),
+                  RoundClock(),
+                  SizedBox(height: MediaQuery.of(context).size.height*.03,),
+                  Text("Timezone",
+                    style: TextStyle(color: Colors.white,fontSize: 18),
+                  ),
+                  SizedBox(height: MediaQuery.of(context).size.height*.015,),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.language,color: Colors.white,size: 17,
+
+                      ),
+                      SizedBox(width: MediaQuery.of(context).size.width*.05,),
+                      Text("UTC"+offsetSign+timeZoneString1+":"+timeZoneString,
+                        style: TextStyle(color: Colors.white,fontSize: 17),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'homepage.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       theme: ThemeData(
         primaryColor: Color(0xFF2D2F41)
